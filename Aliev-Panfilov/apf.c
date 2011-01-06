28349b99a276a1a21e129a7075ca48ea59974710
Hallgeir Lien <hallgeir.lien@gmail.com>
2011-01-06 14:26:19 -0800

    Display of number of threads on startup.

--- Aliev-Panfilov/apf.c
+++ Aliev-Panfilov/apf.c
@@ -134,18 +134,7 @@ int main(int argc, char** argv) {
     m = n;
 
     printTOD("Run begins");
-
-    int nx = (m + 1) / tx, ny = (n + 1) / ty;
-    // The thread geometry must evenly divide N
-    if ((nx * tx != (m + 1)) || (ny * ty != (n + 1)) ) 
-    {
-        printf("Thread geometry: %d x %d\n", tx, ty);
-        printf("The length of the thread geometry axis ");
-        printf("[ %d x %d]\n", tx, ty);
-        printf("  must divide N [%d] evenly\n",m + 1);
-        exit(-1);
-    }
-
+    printf("Max number of threads: %d\n", omp_get_num_threads());
     // Allocate contiguous memory for solution arrays
     // The computational box is defined on [1:m+1,1:n+1]
     // We pad the arrays in order to facilitate differencing on the 

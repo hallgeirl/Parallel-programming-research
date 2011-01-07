037b11f875c4e48a806554b0f87fe3d9069e65f4
Hallgeir Lien <hallgeir.lien@gmail.com>
2011-01-07 13:26:17 -0800

    Threads now only spawn once.

--- Aliev-Panfilov/solve.c
+++ Aliev-Panfilov/solve.c
@@ -13,6 +13,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <stdbool.h>
 //#include <omp.h>
 #include "time.h"
 #include "apf.h"
@@ -20,6 +21,7 @@
 
 typedef struct thread_args_s
 {
+    int thread_id;
     DOUBLE *** E; DOUBLE *** E_prev; DOUBLE ** R;
     DOUBLE alpha;
     int offset_x; int offset_y;
@@ -27,47 +29,114 @@ typedef struct thread_args_s
     DOUBLE dt;
 } thread_args_t;
 
+pthread_barrier_t barr;
+int state = 0;
+int done_count = 0;
+int swap = 0;
+
+pthread_cond_t ready = PTHREAD_COND_INITIALIZER;
+pthread_cond_t done = PTHREAD_COND_INITIALIZER;
+pthread_cond_t swapped = PTHREAD_COND_INITIALIZER;
+
+pthread_mutex_t ready_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t done_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t swapped_mutex = PTHREAD_MUTEX_INITIALIZER;
+
 //Solves the PDE part for one block of the array
 //void solve_pde(DOUBLE ** E, DOUBLE ** E_prev, int offset_x, int offset_y, int bx, int by, int m, int n, DOUBLE alpha)
-void solve_pde(void* _args)
+void solve_block(void* _args)
 {
     thread_args_t* args = (thread_args_t*) _args;
-    DOUBLE ** E = *args->E;
-    DOUBLE ** E_prev = *args->E_prev;
+
+    int thread_id = args->thread_id;
     DOUBLE ** R = args->R;
     DOUBLE alpha = args->alpha;
     int bx = args->bx,  by = args->by;
     int offset_x = args->offset_x,  offset_y = args->offset_y;
     DOUBLE dt = args->dt;
     int i,j;
-    
-    for (i = offset_y; i < offset_y + by; i++)
-    {
-        #pragma ivdep
-        for (j = offset_x; j < offset_x + bx; j++)
-        {
-            E[i][j] = E_prev[i][j] + alpha * (E_prev[i][j + 1]+
-                                      E_prev[i][j - 1]-
-                                      4 * E_prev[i][j]+
-                                      E_prev[i + 1][j]+
-                                      E_prev[i - 1][j]);
-        }
-    }
+    //printf("Thread %d says hello.\n", thread_id);
 
-    for (i = offset_y; i < offset_y + by; i++)
+    while (true)
     {
-        #pragma ivdep
-        for (j = offset_x; j < offset_x + bx; j++)
+        //Wait for border padding
+        pthread_mutex_lock(&ready_mutex);
+        #ifdef DEBUG
+        printf("Thread %d waits for ready.\n", thread_id);
+        #endif
+        while (state == 0)
+            pthread_cond_wait(&ready, &ready_mutex);
+            
+        DOUBLE ** E = *args->E;
+        DOUBLE ** E_prev = *args->E_prev;
+
+        #ifdef DEBUG
+        printf("Thread %d got ready.\n", thread_id);
+        #endif
+        pthread_mutex_unlock(&ready_mutex);
+        
+        for (i = offset_y; i < offset_y + by; i++)
         {
-            E[i][j] += -dt * (kk * E[i][j]*(E[i][j]-a)*(E[i][j] - 1) + E[i][j]*R[i][j]);
-            R[i][j] +=
-                dt * (
-                    epsilon + M1 * R[i][j] /
-                    (E[i][j] + M2)) *
-                    (-R[i][j] - kk * E[i][j] * (E[i][j] - b - 1)
-                );
+            #pragma ivdep
+            for (j = offset_x; j < offset_x + bx; j++)
+            {
+                E[i][j] = E_prev[i][j] + alpha * (E_prev[i][j + 1]+
+                                          E_prev[i][j - 1]-
+                                          4 * E_prev[i][j]+
+                                          E_prev[i + 1][j]+
+                                          E_prev[i - 1][j]);
+            }
         }
+        
+        for (i = offset_y; i < offset_y + by; i++)
+        {
+            #pragma ivdep
+            for (j = offset_x; j < offset_x + bx; j++)
+            {
+                E[i][j] += -dt * (kk * E[i][j]*(E[i][j]-a)*(E[i][j] - 1) + E[i][j]*R[i][j]);
+                R[i][j] +=
+                    dt * (
+                        epsilon + M1 * R[i][j] /
+                        (E[i][j] + M2)) *
+                        (-R[i][j] - kk * E[i][j] * (E[i][j] - b - 1)
+                    );
+            }
+        }
+        
+        //Barrier to make sure no thread waits for ready signal
+        #ifdef DEBUG
+        printf("Thread %d waits at barrier.\n", thread_id);
+        #endif
+        pthread_barrier_wait(&barr);
+        #ifdef DEBUG
+        printf("Thread %d past barrier.\n", thread_id);
+        #endif
+        
+        pthread_mutex_lock(&done_mutex);
+        
+        //Let the first thread entering this lock reset the state variable
+        if (done_count == 0)
+        {
+            #ifdef DEBUG
+            printf("Thread %d sets state to 0.\n", thread_id);
+            #endif
+            state = 0;
+        }
+        
+        //Increment count    
+        done_count++;
+        
+        //Are we done? If so, signal the main thread to continue.
+        if (done_count == 4)
+        {
+            #ifdef DEBUG
+            printf("Thread %d broadcasts done.\n", thread_id);
+            #endif
+            pthread_cond_broadcast(&done);
+        }
+        pthread_mutex_unlock(&done_mutex);
     }
+    printf("Thread %d exits.\n", thread_id);
     pthread_exit(NULL);
 }
 
@@ -85,9 +154,11 @@ int solve(DOUBLE ***_E, DOUBLE ***_E_prev, DOUBLE **R, int m, int n, DOUBLE T, D
 
     DOUBLE **E = *_E, **E_prev = *_E_prev;
 
+    pthread_barrier_init(&barr, NULL, tx*ty);
+    
     //Allocate threads
     pthread_t *threads = (pthread_t*)malloc(sizeof(pthread_t)*tx*ty);
-    
+ 
     //The arguments are fixed for each iteration, so initialize them only once
     thread_args_t *thread_args = (thread_args_t*)malloc(sizeof(thread_args_t)*tx*ty);
     for (ti = 0; ti < ty; ti++)
@@ -104,6 +175,7 @@ int solve(DOUBLE ***_E, DOUBLE ***_E_prev, DOUBLE **R, int m, int n, DOUBLE T, D
             //Let the last thread take care of any leftovers
             if (tj == tx-1) bx += n_mod_tx;
 
+            thread_args[ti*tx+tj].thread_id = ti*tx+tj;
             thread_args[ti*tx+tj].E = &E; 
             thread_args[ti*tx+tj].E_prev = &E_prev;
             thread_args[ti*tx+tj].R = R;
@@ -114,6 +186,9 @@ int solve(DOUBLE ***_E, DOUBLE ***_E_prev, DOUBLE **R, int m, int n, DOUBLE T, D
             thread_args[ti*tx+tj].by = by;
             thread_args[ti*tx+tj].dt = dt;
             
+            
+            //Start the thread
+            pthread_create(&threads[ti*tx+tj], NULL, &solve_block, &thread_args[ti*tx+tj]);
         }
     }
 
@@ -122,6 +197,10 @@ int solve(DOUBLE ***_E, DOUBLE ***_E_prev, DOUBLE **R, int m, int n, DOUBLE T, D
     // This is different from the number of iterations
     while (t < T) 
     {
+        #ifdef DEBUG
+        printf("Main thread starts new loop.\n");
+        #endif
+        
         //printf("Iteration %d\n", niter);
         #ifdef DEBUG
         printMat(E_prev, m, n);
@@ -129,6 +208,7 @@ int solve(DOUBLE ***_E, DOUBLE ***_E_prev, DOUBLE **R, int m, int n, DOUBLE T, D
         if (plot_freq) {
             splot(E_prev, t, niter, m + 1, n + 1, WAIT);
         }
+        printf("\n");
         #endif
 
         t += dt;
@@ -140,7 +220,6 @@ int solve(DOUBLE ***_E, DOUBLE ***_E_prev, DOUBLE **R, int m, int n, DOUBLE T, D
         *
         */
         int i, j;
-        int ii, jj;// for blocking.
 
         #pragma ivdep
         for (j = 1; j <= m + 1; j++) {
@@ -154,26 +233,46 @@ int solve(DOUBLE ***_E, DOUBLE ***_E_prev, DOUBLE **R, int m, int n, DOUBLE T, D
             E_prev[m + 2][i] = E_prev[m][i];
         }
 
+        //Wake up threads to do one iteration worth of work
+        pthread_mutex_lock(&ready_mutex);
+        #ifdef DEBUG
+        printf("Main thread inited ghost cells, sets state to 1 and broadcasts ready.\n");
+        #endif
+        state = 1;
+        pthread_cond_broadcast(&ready);
+        pthread_mutex_unlock(&ready_mutex);
+
+        //Wait for threads to finish
+        pthread_mutex_lock(&done_mutex);
+        #ifdef DEBUG
+        printf("Main waits for done signal.\n");
+        #endif
+        while (done_count < tx*ty)
+            pthread_cond_wait(&done, &done_mutex);
+        #ifdef DEBUG
+        printf("Main received done signal, sets done_count to 0.\n");
+        #endif
+        done_count = 0;
+        pthread_mutex_unlock(&done_mutex);
+
         //Main loop
-        for (ti = 0; ti < ty; ti++)
+        /*for (ti = 0; ti < ty; ti++)
         {
             for (tj = 0; tj < tx; tj++)
             {
                 //Create thread and execute solver for sub-problem
-                pthread_create(&threads[ti*tx+tj], NULL, &solve_pde, &thread_args[ti*tx+tj]);
+                pthread_create(&threads[ti*tx+tj], NULL, &solve_block, &thread_args[ti*tx+tj]);
             }
-        }
+        }*/
         
-        //pthread_barrier_wait(&barrier);
-        //pthread_join(threads[0], NULL);
         //Join the threads
-        for (ti = 0; ti < ty; ti++)
+        /*for (ti = 0; ti < ty; ti++)
         {
             for (tj = 0; tj < tx; tj++)
             {
                 pthread_join(threads[ti*tx+tj], NULL);
             }
-        }
+        }*/
 
         if (do_stats) {
             repNorms(E, t, dt, m, n, niter);
@@ -191,6 +290,12 @@ int solve(DOUBLE ***_E, DOUBLE ***_E_prev, DOUBLE **R, int m, int n, DOUBLE T, D
         DOUBLE **tmp = E;
         E = E_prev;
         E_prev = tmp;
+        
+        //Tell the threads that the arrays are swapped
+        /*pthread_mutex_lock(&swapped_mutex);
+        swap = 1;
+        pthread_cond_broadcast(&swapped);
+        pthread_mutex_unlock(&swapped_mutex);*/
     }
 
     // Store them into the pointers passed in
